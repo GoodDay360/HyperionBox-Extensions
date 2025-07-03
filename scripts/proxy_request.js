477ab597939dd4fda129e734838e39d77a9e8752
unknown <istartgame31@gmail.com>
2025-07-03 13:27:49 +0700

    fix proxy request encoding. cause stream to error

--- scripts/proxy_request.js
+++ scripts/proxy_request.js
@@ -2,15 +2,29 @@ import fetch from "node-fetch";
 
 const proxy_request = async ({ url, referer, headers }) => {
     try {
+        console.log("=================")
+        console.log(referer);
         console.log(url);
+        console.log("=================")
 
-        const allowedHeaders = ["Accept", "Accept-Language", "Range"];
+        const allowedPatterns = [
+            /^Accept$/,
+            /^Accept-Language$/,
+            /^Range$/,
+            /^Content-Type$/,
+            /^Accept-Ranges$/,
+            /^Content-Length$/,
+            /^Access-Control-.+$/,  // Matches anything starting with Access-Control-
+            /^X-.+$/                // Matches headers like X-Custom-Header, X-Requested-With, etc.
+        ];
+
         const sanitizedHeaders = Object.keys(headers)
-            .filter(key => allowedHeaders.includes(key))
-            .reduce((obj, key) => {
-                obj[key] = headers[key];
-                return obj;
-            }, {});
+        .filter(key => allowedPatterns.some(pattern => pattern.test(key)))
+        .reduce((obj, key) => {
+            obj[key] = headers[key];
+            return obj;
+        }, {});
+
 
         
         const response = await fetch(url, {
@@ -25,9 +39,13 @@ const proxy_request = async ({ url, referer, headers }) => {
         if (!response.ok) {
             throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
         }
+        
 
         const responseHeaders = Object.fromEntries(response.headers.entries());
+        delete responseHeaders['content-encoding'];
+
         const bodyStream = response.body;
+        console.log(responseHeaders);
 
         return { data: bodyStream, headers: responseHeaders };
 
